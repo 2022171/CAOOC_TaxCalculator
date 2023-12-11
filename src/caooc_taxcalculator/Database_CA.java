f73f9a4547935ddd044c907a997bfbad47d8336b
C2022188 <2022188@student.cct.ie>
2023-12-11 13:32:50 +0000

    Create Database_CA.java
    Setup Database

---
+++ src/caooc_taxcalculator/Database_CA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caooc_taxcalculator;
+
+/**
+ *
+ * @author caroo
+ */
+public abstract class Database_CA {
+    
+    protected final static String DB_BASE_URL = "jdbc:mysql://localhost";
+    protected final static String USER = "ooc2023";
+    protected final static String PASSWORD = "ooc2023";
+    protected final static String DB_NAME = "CA_TaxCalculator";
+    protected final static String TABLE_NAME = "usersData";
+    protected final static String DB_URL = DB_BASE_URL + "/" + DB_NAME;
+    
+}
