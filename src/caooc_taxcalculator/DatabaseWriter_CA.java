e77686ac2a6ea6966028aebe30f3c49c61b536c8
C2022188 <2022188@student.cct.ie>
2023-12-11 13:44:24 +0000

    Setup Database

---
+++ src/caooc_taxcalculator/DatabaseWriter_CA.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package caooc_taxcalculator;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+/**
+ *
+ * @author caroo
+ */
+public class DatabaseWriter_CA extends Database_CA {
+    
+    public boolean newUser(Users users) throws SQLException {
+        try(
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            Statement stmt = conn.createStatement();  
+        ){
+            String sql = String.format("INSERT INTO " + TABLE_NAME + " VALUES ("
+                    + "'%d', '%s', '%s', '%s', '%s', '%f', '%f', '%f', '%s', '%s');",
+                    users.getID(), users.getFirstName(), users.getLastName(), users.getEmail(), users.getPPSN(), users.getGrossIncome(), 
+                    users.getTaxCredit(), users.getTaxOwed(), users.getUsername(), users.getPassword());
+            stmt.execute(sql);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    public boolean addAllUsers(List<Users> usersList) {
+        return true;
+    }
+}
+    
+    
--- src/caooc_taxcalculator/Database_CA.java
+++ src/caooc_taxcalculator/Database_CA.java
@@ -9,7 +9,7 @@ package caooc_taxcalculator;
  * @author caroo
  */
 public abstract class Database_CA {
-    
+    //to comunicate with database
     protected final static String DB_BASE_URL = "jdbc:mysql://localhost";
     protected final static String USER = "ooc2023";
     protected final static String PASSWORD = "ooc2023";
