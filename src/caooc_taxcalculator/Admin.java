7f8e90a7263bdb98003fdb1b090238aa4d21cacf
2022171 <146198931+2022171@users.noreply.github.com>
2023-12-11 13:22:11 +0000

    Update Admin.java

--- src/caooc_taxcalculator/Admin.java
+++ src/caooc_taxcalculator/Admin.java
@@ -4,10 +4,48 @@
  */
 package caooc_taxcalculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author danie
  */
-public class Admin {
+public class Admin extends Users {
+    
+public void modifyOwnProfile() {
+        // Modifying own profile details
+       // setFirstName(newFirstName);
+      //  setLastName(newLastName);
+        
+        // Any other attributes you want to modify
+    }
+
+    // Method to access a list of all other users in the system
+    public List<Users> getAllUsers(List<Users> allUsers) {
+        List<Users> otherUsers = new ArrayList<>(allUsers);
+        otherUsers.remove(this); // Remove the manager from the list
+        return otherUsers;
+    }
+
+    // Method to remove other users from the system
+    public void removeUser(List<Users> allStaff, Users userToRemove) {
+        if (allStaff.contains(userToRemove)) {
+            allStaff.remove(userToRemove);
+            System.out.println(userToRemove.getFirstName() + " " + userToRemove.getLastName() + " removed from the system.");
+        } else {
+            System.out.println("User not found in the system.");
+        }
+    }
+
+    // Method to review the operations performed by other users
+    public void reviewUserOperations(Users user) {
+        // Implement logic to review operations performed by a specific user
+        // This could involve accessing logs, database entries, etc.
+        // For example:
+        // user.getOperationsPerformed();
+        System.out.println("Reviewing operations performed by " + user.getFirstName() + " " + user.getLastName());
+    }
+        
     
 }
